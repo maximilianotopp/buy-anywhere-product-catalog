e1683b097e11061eb961753076bb0c346a9b3942
Cristian Sánchez <sanchezcristianm@gmail.com>
2019-01-08 23:46:33 -0300

    @JsonIgnore added at the field level.

--- src/main/java/com/buyanywhere/productcatalog/models/Category.java
+++ src/main/java/com/buyanywhere/productcatalog/models/Category.java
@@ -1,5 +1,7 @@
 package com.buyanywhere.productcatalog.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 
 @Entity
@@ -15,6 +17,7 @@ public class Category {
     private int displayOrder;
 
     @Column(nullable = false)
+    @JsonIgnore
     private boolean deleted;
 
     public Category(){
