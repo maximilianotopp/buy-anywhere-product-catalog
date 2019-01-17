815514b64f79621bd576945bd1c3ad7c3ea5b888
ramiro-gioia <rgioia@belatrixsf.com>
2019-01-17 00:30:24 -0300

    CreateCategory - Initial implementation

--- src/main/java/com/buyanywhere/productcatalog/controllers/v1/CategoryController.java
+++ src/main/java/com/buyanywhere/productcatalog/controllers/v1/CategoryController.java
@@ -1,1 +1,28 @@
+package com.buyanywhere.productcatalog.controllers.v1;
 
+import com.buyanywhere.productcatalog.models.Category;
+import com.buyanywhere.productcatalog.repositories.CategoryRepository;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/v1/category")
+
+public class CategoryController{
+
+    private CategoryRepository repository;
+
+    public CategoryController(CategoryRepository repository){
+        this.repository = repository;
+
+    }
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            value = "/"
+    )
+    public Category post(@RequestBody Category category){
+        return repository.save(category);
+    }
+
+}
