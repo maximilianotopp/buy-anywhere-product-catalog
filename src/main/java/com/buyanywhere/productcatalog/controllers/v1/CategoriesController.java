a8c37f705e7e68825b7b5b513d6cb4902be188c8
Cristian Sánchez <sanchezcristianm@gmail.com>
2019-01-20 16:37:57 -0300

    Return categories completed.

--- src/main/java/com/buyanywhere/productcatalog/controllers/v1/CategoriesController.java
+++ src/main/java/com/buyanywhere/productcatalog/controllers/v1/CategoriesController.java
@@ -1,19 +1,18 @@
 package com.buyanywhere.productcatalog.controllers.v1;
 
+import com.buyanywhere.productcatalog.exceptions.ArgumentNotValidException;
 import com.buyanywhere.productcatalog.models.Category;
 import com.buyanywhere.productcatalog.repositories.CategoryRepository;
-import com.buyanywhere.productcatalog.specifications.CategorySpecificationsBuilder;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.*;
+import java.util.*;
 
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping("/v1/categories")
 public class CategoriesController {
     private CategoryRepository repository;
+
 
     public CategoriesController(CategoryRepository repository) {
         this.repository = repository;
@@ -23,14 +22,50 @@ public class CategoriesController {
             method = RequestMethod.GET
     )
     public List<Category> get(@RequestParam(value = "filterBy") String filterBy,
-                              @RequestParam(value = "showDeleted", defaultValue = "false") boolean showDeleted){
+                              @RequestParam(value = "showDeleted", defaultValue = "false") boolean showDeleted,
+                              @RequestParam(value = "orderBy", defaultValue = "alpha") String orderBy,
+                              @RequestParam(value = "reverse", defaultValue = "false") boolean reverse){
 
-        Specification<Category> spec = Specification.where((root, criteriaQuery, criteriaBuilder) -> {
-            return criteriaBuilder.like(
-                    root.<String>get("name"), "%" + filterBy + "%");
-        }).and((root, criteriaQuery, criteriaBuilder) -> {
+        Specification<Category> spec;
 
-        });
-        return repository.findAll();
+        if(!(orderBy.equals("alpha") || orderBy.equals("order"))) {
+            throw new ArgumentNotValidException("orderBy");
+        }
+
+        if (filterBy.length() > 3){
+            spec = Specification.where((root, criteriaQuery, criteriaBuilder) -> {
+                return criteriaBuilder.like(
+                        root.<String>get("name"), "%" + filterBy + "%");
+            });
+        } else {
+            spec = Specification.where((root, criteriaQuery, criteriaBuilder) -> {
+                return criteriaBuilder.like(
+                        root.<String>get("name"), "%");
+            });
+        }
+
+        if (!showDeleted) {
+            spec = Specification.where(spec).and((root, criteriaQuery, criteriaBuilder) -> {
+                return criteriaBuilder.equal(
+                        root.<String>get("deleted"), 0);
+            });
+        }
+
+        List<Category> searchResults = repository.findAll(spec);
+
+        Comparator<Category> categoryComparator;
+        if(orderBy.equals("order")){
+            categoryComparator = Comparator.comparing(Category::getDisplayOrder);
+        } else {
+            categoryComparator = Comparator.comparing(Category::getName);
+        }
+
+        if(reverse){
+            Collections.sort(searchResults, categoryComparator.reversed());
+        } else {
+            Collections.sort(searchResults, categoryComparator);
+        }
+
+        return searchResults;
     }
 }
