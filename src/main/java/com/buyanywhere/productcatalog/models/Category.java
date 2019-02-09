22680125a8b140194d67b88dc4b32c4ffaad4095
MarceloMansilla <mansillamarcelo90@gmail.com>
2019-02-09 14:34:50 -0300

    Creating Category - removing parameters for the annotation @GeneratedValue

--- src/main/java/com/buyanywhere/productcatalog/models/Category.java
+++ src/main/java/com/buyanywhere/productcatalog/models/Category.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 @Entity
 public class Category {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue
 	private int id;
 	private String name;
 	private int displayOrder;
