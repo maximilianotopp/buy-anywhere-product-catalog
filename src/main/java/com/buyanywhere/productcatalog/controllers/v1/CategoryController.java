c4b992b30e98d18d6e10939dfe23ef0f96f955f9
RomiZelaya <rominazelayaunlam@gmail.com>
2019-02-12 00:47:02 -0300

    Create Category completed

--- src/main/java/com/buyanywhere/productcatalog/controllers/v1/CategoryController.java
+++ src/main/java/com/buyanywhere/productcatalog/controllers/v1/CategoryController.java
@@ -1,1 +1,27 @@
+package com.buyanywhere.productcatalog.controllers.v1;
 
+import com.buyanywhere.productcatalog.models.Category;
+import com.buyanywhere.productcatalog.models.Greeting;
+import com.buyanywhere.productcatalog.repositories.CategoryRepository;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/category")
+public class CategoryController{
+    private CategoryRepository repository;
+
+    public CategoryController(CategoryRepository repository) {
+        this.repository = repository;
+    }
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            value = "/"
+    )
+    public Category post(@RequestBody Category category){
+        return repository.save(category);
+    }
+}
