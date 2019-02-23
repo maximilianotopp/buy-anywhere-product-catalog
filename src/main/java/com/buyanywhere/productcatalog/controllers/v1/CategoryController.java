b88675bd35a5566f5a6eb2fbee8ac8f11ce6b87c
Cristian Sánchez <sanchezcristianm@gmail.com>
2019-02-22 23:28:57 -0300

    Changed @RequestMapping for composed annotations.

--- src/main/java/com/buyanywhere/productcatalog/controllers/v1/CategoryController.java
+++ src/main/java/com/buyanywhere/productcatalog/controllers/v1/CategoryController.java
@@ -16,7 +16,7 @@ public class CategoryController {
         this.repository = repository;
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+    @GetMapping(value = "/{id}")
     public Category get(@PathVariable long id) throws CategoryNotFoundException{
         if(!exists(id)){
             throw new CategoryNotFoundException(id);
@@ -25,7 +25,7 @@ public class CategoryController {
         return repository.findById(id).get();
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
     public Category post(@RequestBody Category category) throws CategoryNotValidException{
         if(!category.isValid()){
             throw new CategoryNotValidException(this.getInvalidFields(category));
@@ -34,7 +34,7 @@ public class CategoryController {
         return repository.save(category);
     }
 
-    @RequestMapping(method = RequestMethod.PUT)
+    @PutMapping
     public Category put(@RequestBody Category category) throws CategoryNotValidException, CategoryNotFoundException {
         if(!category.isValid()){
             throw new CategoryNotValidException(this.getInvalidFields(category));
@@ -47,7 +47,7 @@ public class CategoryController {
         return repository.save(category);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
+    @DeleteMapping(value = "/{id}")
     public void delete(@PathVariable long id) throws CategoryNotFoundException{
         if(!exists(id)){
             throw new CategoryNotFoundException(id);
