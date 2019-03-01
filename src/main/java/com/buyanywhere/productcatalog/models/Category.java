b32cacc7aab6e989cc8afa36de1a284fd8955bca
cdomancich <cami.domancich@gmail.com>
2019-03-01 19:24:30 -0300

    First card Create Category completed

--- src/main/java/com/buyanywhere/productcatalog/models/Category.java
+++ src/main/java/com/buyanywhere/productcatalog/models/Category.java
@@ -2,51 +2,39 @@ package com.buyanywhere.productcatalog.models;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Category {
-
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue
     private int id;
     private String name;
     private int displayOrder;
-    private boolean deleted = false;
+    private boolean deleted;
+
+    public Category(String name, int displayOrder, boolean deleted) {
+        this.name = name;
+        this.displayOrder = displayOrder;
+        this.deleted = deleted;
+    }
+
+    public Category() {
+    }
 
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getDisplayOrder() {
         return displayOrder;
-    }
-
-    public void setDisplayOrder(int displayOrder) {
-        this.displayOrder = displayOrder;
     }
 
     public boolean isDeleted() {
         return deleted;
     }
-
-    public void setDeleted(boolean deleted) {
-        this.deleted = deleted;
-    }
-
-
-
 }
