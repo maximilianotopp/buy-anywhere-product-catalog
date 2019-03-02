23c2864ede562ad6ea8a02045e3e68f95e6f4e09
cdomancich <cami.domancich@gmail.com>
2019-03-02 15:04:20 -0300

    Delete an import from 'ArgumentNotValidException'

--- src/main/java/com/buyanywhere/productcatalog/exceptions/ArgumentNotValidException.java
+++ src/main/java/com/buyanywhere/productcatalog/exceptions/ArgumentNotValidException.java
@@ -1,6 +1,5 @@
 package com.buyanywhere.productcatalog.exceptions;
 
-import com.buyanywhere.productcatalog.models.Category;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
