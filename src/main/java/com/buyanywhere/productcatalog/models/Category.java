4eebb0003cf654718ce690423853724a02c21cf6
RomiZelaya <rominazelayaunlam@gmail.com>
2019-03-03 22:26:02 -0300

    PR fixed-long to Long in Category Class

--- src/main/java/com/buyanywhere/productcatalog/models/Category.java
+++ src/main/java/com/buyanywhere/productcatalog/models/Category.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 public class Category{
     @Id
     @GeneratedValue
-    private long id;
+    private Long id;
     private String name;
     private int displayOrder;
     @JsonIgnore
