7396a5597525b85b9c9cb6aea693b42d6e6d393c
nmelo <melonahuel@hotmail.com>
2019-03-06 15:55:02 -0300

    Delete Category. Fixed "Long" insted of "long" in CategoryNotFoundException.

--- src/main/java/com/buyanywhere/productcatalog/exceptions/CategoryNotFoundException.java
+++ src/main/java/com/buyanywhere/productcatalog/exceptions/CategoryNotFoundException.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class CategoryNotFoundException extends RuntimeException {
-    public CategoryNotFoundException(long id){
+    public CategoryNotFoundException(Long id){
         super("No category found with Id:" + id);
     }
 }
