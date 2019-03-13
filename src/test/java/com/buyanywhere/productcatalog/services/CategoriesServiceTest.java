0f1c19bfb2b8ea2a06dfaa967145fbc22bb93cb4
mtopp <mtopp@belatrixsf.com>
2019-03-13 09:20:12 -0300

    Added test cases

--- src/test/java/com/buyanywhere/productcatalog/services/CategoriesServiceTest.java
+++ src/test/java/com/buyanywhere/productcatalog/services/CategoriesServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.mockito.internal.util.reflection.FieldSetter;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -33,6 +34,9 @@ public class CategoriesServiceTest {
         deletedCategory.delete();
         categories.add(deletedCategory);
 
+        Category notDeletedCategory = new Category("NotDeletedCategory", 6);
+        categories.add(notDeletedCategory);
+
         Mockito.when(categoryRepository.findAll()).thenReturn(categories);
         Mockito.when(categoryRepository.findById((long) 1)).thenReturn(Optional.ofNullable(categories.get(0)));
         Mockito.when(categoryRepository.findById((long) 2)).thenReturn(Optional.ofNullable(categories.get(1)));
@@ -43,6 +47,14 @@ public class CategoriesServiceTest {
         Mockito.when(categoryRepository.findByNameAndDeletedFalse("NewName")).thenReturn(new ArrayList<>());
         Mockito.when(categoryRepository.findByNameAndDeletedFalse("Cars")).thenReturn(new ArrayList<>(Collections.singleton(categories.get(1))));
         Mockito.when(categoryRepository.findByNameAndDeletedFalse("DeletedCategory")).thenReturn(new ArrayList<>());
+
+        Mockito.when(categoryRepository.findByNameAndDeletedFalse("NotDeletedCategory")).thenReturn(new ArrayList<>(Collections.singleton(categories.get(5))));
+
+        try {
+            FieldSetter.setField(notDeletedCategory, notDeletedCategory.getClass().getDeclaredField("id"), (long) 6);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
@@ -67,5 +79,61 @@ public class CategoriesServiceTest {
         CategoriesService service = new CategoriesService(categoryRepository);
 
         Category category = service.findById((long) 6);
+    }
+
+    @Test
+    public void exists_whenExistingIdAndNotDeleted_shouldReturnTrue(){
+        CategoriesService service = new CategoriesService(categoryRepository);
+
+        Assert.assertTrue(service.exists((long) 3));
+    }
+
+    @Test
+    public void exists_whenExistingIdAndDeleted_shouldReturnFalse(){
+        CategoriesService service = new CategoriesService(categoryRepository);
+
+        Assert.assertFalse(service.exists((long) 5));
+    }
+
+    @Test
+    public void exists_whenNonExistingId_shouldReturnFalse(){
+        CategoriesService service = new CategoriesService(categoryRepository);
+
+        Assert.assertFalse(service.exists((long) 6));
+    }
+
+    @Test
+    public void isDuplicated_withNameOnly_whenNoDuplicatedNames_shouldReturnFalse(){
+        CategoriesService service = new CategoriesService(categoryRepository);
+
+        Assert.assertFalse(service.isDuplicated("NewName"));
+    }
+
+    @Test
+    public void isDuplicated_withNameOnly_whenDuplicatedNames_shouldReturnTrue(){
+        CategoriesService service = new CategoriesService(categoryRepository);
+
+        Assert.assertTrue(service.isDuplicated("Cars"));
+    }
+
+    @Test
+    public void isDuplicated_withIdAndName_whenNoDuplicatedNames_shouldReturnFalse(){
+        CategoriesService service = new CategoriesService(categoryRepository);
+
+        Assert.assertFalse(service.isDuplicated((long) 9, "NewName"));
+    }
+
+    @Test
+    public void isDuplicated_withIdAndName_whenDuplicatedNamesAndDifferentId_shouldReturnTrue(){
+        CategoriesService service = new CategoriesService(categoryRepository);
+
+        Assert.assertTrue(service.isDuplicated((long) 5, "NotDeletedCategory"));
+    }
+
+    @Test
+    public void isDuplicated_withIdAndName_whenDuplicatedNamesAndDifferentId_butDuplicatedIsDeleted_shouldReturnFalse(){
+        CategoriesService service = new CategoriesService(categoryRepository);
+
+        Assert.assertFalse(service.isDuplicated((long) 5, "DeletedCategory"));
     }
 }
