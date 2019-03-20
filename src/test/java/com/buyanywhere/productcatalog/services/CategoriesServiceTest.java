3c6db0ad9933dcf17e0ba4895ff81b5b6f12707e
sanchezcristian <sanchezcristianm@gmail.com>
2019-03-20 19:30:34 -0300

    CategoriesServiceTests refactoring.

--- src/test/java/com/buyanywhere/productcatalog/services/CategoriesServiceTest.java
+++ src/test/java/com/buyanywhere/productcatalog/services/CategoriesServiceTest.java
@@ -4,7 +4,6 @@ import com.buyanywhere.productcatalog.exceptions.CategoryNotFoundException;
 import com.buyanywhere.productcatalog.models.Category;
 import com.buyanywhere.productcatalog.repositories.ICategoryRepository;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -14,7 +13,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
 
 @RunWith(SpringRunner.class)
@@ -22,77 +20,70 @@ public class CategoriesServiceTest {
     @MockBean
     private ICategoryRepository categoryRepository;
 
-    @Before
-    public void setup(){
-        List<Category> categories = new ArrayList<>();
-        categories.add(new Category("Home", 1));
-        categories.add(new Category("Cars", 2));
-        categories.add(new Category("Electronics", 3));
-        categories.add(new Category("Computers", 4));
-
-        Category deletedCategory = new Category("DeletedCategory", 5);
-        deletedCategory.delete();
-        categories.add(deletedCategory);
-
-        Category notDeletedCategory = new Category("NotDeletedCategory", 6);
-        categories.add(notDeletedCategory);
-
-        Mockito.when(categoryRepository.findAll()).thenReturn(categories);
-        Mockito.when(categoryRepository.findById((long) 1)).thenReturn(Optional.ofNullable(categories.get(0)));
-        Mockito.when(categoryRepository.findById((long) 2)).thenReturn(Optional.ofNullable(categories.get(1)));
-        Mockito.when(categoryRepository.findById((long) 3)).thenReturn(Optional.ofNullable(categories.get(2)));
-        Mockito.when(categoryRepository.findById((long) 4)).thenReturn(Optional.ofNullable(categories.get(3)));
-        Mockito.when(categoryRepository.findById((long) 5)).thenReturn(Optional.ofNullable(categories.get(4)));
-
-        Mockito.when(categoryRepository.findByNameAndDeletedFalse("NewName")).thenReturn(new ArrayList<>());
-        Mockito.when(categoryRepository.findByNameAndDeletedFalse("Cars")).thenReturn(new ArrayList<>(Collections.singleton(categories.get(1))));
-        Mockito.when(categoryRepository.findByNameAndDeletedFalse("DeletedCategory")).thenReturn(new ArrayList<>());
-
-        Mockito.when(categoryRepository.findByNameAndDeletedFalse("NotDeletedCategory")).thenReturn(new ArrayList<>(Collections.singleton(categories.get(5))));
-
-        try {
-            FieldSetter.setField(notDeletedCategory, notDeletedCategory.getClass().getDeclaredField("id"), (long) 6);
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        }
-    }
-
     @Test
     public void findById_whenExistingIdAndNotDeleted_shouldReturnCategory(){
+        final long id = 3;
+        final String name = "Electronics";
+        final int displayOrder = 7;
+
         CategoriesService service = new CategoriesService(categoryRepository);
 
-        Category category = service.findById((long) 3);
+        Category category = new Category(name, displayOrder);
+        SetField(category, "id", id);
 
-        Assert.assertEquals("Electronics", category.getName());
-        Assert.assertEquals((long) 3, category.getDisplayOrder());
+        MockFindById(id, category);
+
+        Category categoryResult = service.findById(id);
+
+        Assert.assertEquals(name, categoryResult.getName());
+        Assert.assertEquals(displayOrder, categoryResult.getDisplayOrder());
     }
 
     @Test(expected = CategoryNotFoundException.class)
     public void findById_whenExistingIdAndDeleted_shouldThrowCategoryNotFoundException(){
+        final long id = 5;
+
         CategoriesService service = new CategoriesService(categoryRepository);
 
-        Category category = service.findById((long) 5);
+        Category deletedCategory = new Category("DeletedCategory", 5);
+        deletedCategory.delete();
+        SetField(deletedCategory, "id", id);
+
+        MockFindById(id, deletedCategory);
+
+        service.findById(id);
     }
 
     @Test(expected = CategoryNotFoundException.class)
     public void findById_whenNonExistingId_shouldThrowCategoryNotFoundException(){
         CategoriesService service = new CategoriesService(categoryRepository);
 
-        Category category = service.findById((long) 6);
+        service.findById((long) 6);
     }
 
     @Test
     public void exists_whenExistingIdAndNotDeleted_shouldReturnTrue(){
+        final long id = 3;
         CategoriesService service = new CategoriesService(categoryRepository);
 
-        Assert.assertTrue(service.exists((long) 3));
+        Category category = new Category("Electronics", 1);
+
+        MockFindById(id, category);
+
+        Assert.assertTrue(service.exists(id));
     }
 
     @Test
     public void exists_whenExistingIdAndDeleted_shouldReturnFalse(){
+        final long id = 5;
         CategoriesService service = new CategoriesService(categoryRepository);
 
-        Assert.assertFalse(service.exists((long) 5));
+        Category deletedCategory = new Category("Computers", 4);
+        deletedCategory.delete();
+
+        MockFindById(id, deletedCategory);
+
+        Assert.assertFalse(service.exists(id));
     }
 
     @Test
@@ -104,36 +95,73 @@ public class CategoriesServiceTest {
 
     @Test
     public void isDuplicated_withNameOnly_whenNoDuplicatedNames_shouldReturnFalse(){
+        final String name = "NewName";
         CategoriesService service = new CategoriesService(categoryRepository);
 
-        Assert.assertFalse(service.isDuplicated("NewName"));
+        MockFindByNameAndDeletedFalse(name, new ArrayList<>());
+
+        Assert.assertFalse(service.isDuplicated(name));
     }
 
     @Test
     public void isDuplicated_withNameOnly_whenDuplicatedNames_shouldReturnTrue(){
+        final String name = "Cars";
+
         CategoriesService service = new CategoriesService(categoryRepository);
 
-        Assert.assertTrue(service.isDuplicated("Cars"));
+        Category category = new Category(name, 2);
+
+        MockFindByNameAndDeletedFalse(name, new ArrayList<>(Collections.singleton(category)));
+
+        Assert.assertTrue(service.isDuplicated(name));
     }
 
     @Test
     public void isDuplicated_withIdAndName_whenNoDuplicatedNames_shouldReturnFalse(){
+        final String name = "NewName";
         CategoriesService service = new CategoriesService(categoryRepository);
 
-        Assert.assertFalse(service.isDuplicated((long) 9, "NewName"));
+        MockFindByNameAndDeletedFalse(name, new ArrayList<>());
+
+        Assert.assertFalse(service.isDuplicated((long) 9, name));
     }
 
     @Test
     public void isDuplicated_withIdAndName_whenDuplicatedNamesAndDifferentId_shouldReturnTrue(){
+        final String name = "NotDeletedCategory";
         CategoriesService service = new CategoriesService(categoryRepository);
 
-        Assert.assertTrue(service.isDuplicated((long) 5, "NotDeletedCategory"));
+        Category notDeletedCategory = new Category(name, 4);
+        SetField(notDeletedCategory, "id", (long) 6);
+
+        MockFindByNameAndDeletedFalse(name, new ArrayList<>(Collections.singleton(notDeletedCategory)));
+
+        Assert.assertTrue(service.isDuplicated((long) 5, name));
     }
 
     @Test
     public void isDuplicated_withIdAndName_whenDuplicatedNamesAndDifferentId_butDuplicatedIsDeleted_shouldReturnFalse(){
+        final String name = "DeletedCategory";
         CategoriesService service = new CategoriesService(categoryRepository);
 
-        Assert.assertFalse(service.isDuplicated((long) 5, "DeletedCategory"));
+        MockFindByNameAndDeletedFalse(name, new ArrayList<>());
+
+        Assert.assertFalse(service.isDuplicated((long) 5, name));
+    }
+
+    private void MockFindById(long id, Category returnCategory){
+        Mockito.when(categoryRepository.findById(id)).thenReturn(Optional.ofNullable(returnCategory));
+    }
+
+    private void MockFindByNameAndDeletedFalse(String name, ArrayList<Category> categories){
+        Mockito.when(categoryRepository.findByNameAndDeletedFalse(name)).thenReturn(categories);
+    }
+
+    private void SetField(Category category, String fieldName, Object value){
+        try {
+            FieldSetter.setField(category, category.getClass().getDeclaredField(fieldName), value);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
     }
 }
